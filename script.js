e805291dd9d7ff1a0d9d83a745487c0f5474cc78
aistyte111 <109670714+aistyte111@users.noreply.github.com>
2023-11-16 15:17:43 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-const buttons = document.querySelectorAll("[data-carousel-button]")
+document.addEventListener("DOMContentLoaded", function () {
+    const carousel = document.querySelector("[data-carousel]");
+    const slides = document.querySelectorAll(".slide");
+    const prevButton = document.querySelector("[data-carousel-button='prev']");
+    const nextButton = document.querySelector("[data-carousel-button='next']");
+    let currentIndex = 0;
 
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        const offset = button.dataset.carouselButton === "next" ? 1 : -1
-        const slides = button.closest ("[data-carousel]")
-            .querySelector("[data-slides]")
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            if (i === index) {
+                slide.style.display = "block";
+            } else {
+                slide.style.display = "none";
+            }
+        });
+    }
 
-            const activeSlide = slides.querySelector("[data-active]")
-            let newIndex = [...slides.children].indexOf(activeSlide) + offset
-            if (newIndex < 0) newIndex = slides.children.length -1
-            if (newIndex >= slides.cchildren.length) newIndex = 0
+    function showPrevSlide() {
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        showSlide(currentIndex);
+    }
 
-            slides.children[newIndex].dataset.active = true
-            delete activeSlide.dataset.active
-    })
-})
+    function showNextSlide() {
+        currentIndex = (currentIndex + 1) % slides.length;
+        showSlide(currentIndex);
+    }
+
+    prevButton.addEventListener("click", showPrevSlide);
+    nextButton.addEventListener("click", showNextSlide);
+
+    // Show the initial slide
+    showSlide(currentIndex);
+});
